f558a7970ba50cf4dbdcd89bcf6363b55fe0ea9b
terdia <terrymarcy2000@yahoo.com>
2021-09-09 09:13:22 +0200

    cleanup healthcheck

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -6,7 +6,11 @@ import (
 )
 
 func (app *application) healthcheckHandler(rw http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(rw, "status: available")
-	fmt.Fprintf(rw, "environment: %s\n", app.config.env)
-	fmt.Fprintf(rw, "version: %s\n", version)
+
+	js := `{"status": "available", "environment": %q, "version": %q}`
+	js = fmt.Sprintf(js, app.config.env, version)
+
+	rw.Header().Set("Content-Type", "application/json")
+
+	rw.Write([]byte(js))
 }
